c790892fea927139cb92635fd5fcaa36a5782da3
buihaison <buihaison21@gmail.com>
2024-03-14 17:47:22 +0700

    []

--- controllers/fix_rpc.py
+++ controllers/fix_rpc.py
@@ -2,12 +2,8 @@ import xmlrpc.client
 from odoo.http import Controller, dispatch_rpc, request
 from odoo.addons.base.controllers.rpc import RPC
 
-import logging
-_logger = logging.getLogger(__name__)
-
 class RPC(Controller):
     def _xmlrpc(self, service):
-        _logger.info("AAAAAAAAAAAA", False, False)
         """Common method to handle an XML-RPC request."""
         data = request.httprequest.get_data()
         params, method = xmlrpc.client.loads(data)
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,1 @@
 #from . import action_apply_inventory_rpc
-from . import fix_rpc
