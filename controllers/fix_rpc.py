86e1d358cfd00941814b5e165b19e6d41411b746
buihaison <buihaison21@gmail.com>
2024-03-14 17:52:13 +0700

    []

--- controllers/fix_rpc.py
+++ controllers/fix_rpc.py
@@ -4,8 +4,10 @@ from odoo.addons.base.controllers.rpc import RPC
 
 class RPC(Controller):
     def _xmlrpc(self, service):
+        '''
         """Common method to handle an XML-RPC request."""
         data = request.httprequest.get_data()
         params, method = xmlrpc.client.loads(data)
         result = dispatch_rpc(service, method, params)
         return xmlrpc.client.dumps((result,), methodresponse=1, allow_none=True)
+        '''
